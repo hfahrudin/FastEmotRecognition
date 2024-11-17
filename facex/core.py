88d98a4bbc7e3438ad30aaf635ad2b000cda2b99
hfahrudin <fahrudinhasby12@gmail.com>
2024-11-17 21:19:34 +0900

    implement pooling method

--- facex/core.py
+++ facex/core.py
@@ -2,31 +2,110 @@
 import numpy as np
 import pathlib
 import os
-from facex.utils import _preprocess_img, _parse_result
+from concurrent.futures import ThreadPoolExecutor
+import threading
+from facex.utils import create_interpreter, parse_result, input_resize
 
-weight = os.path.join(pathlib.Path(__file__).parent.absolute(), "model_optimized.tflite")
+class EmotionClassifier:
+    """
+    A worker for predicting emotions.
+    """
+    def __init__(self, model_path):
+        """
+        Initialize the EmotionClassifier with the given model path.
+        
+        Args:
+            model_path (str): Path to the pre-trained model.
+        """
+        self.model = create_interpreter(model_path)
+        self.input_details = self.model.get_input_details()
+        self.output_details = self.model.get_output_details()
 
-import numpy as np
-import tensorflow as tf
+    def predict(self, input):
+        """
+        Predict the emotion based on the input data.
+        
+        Args:
+            input (np.ndarray): Input data for emotion classification.
+        
+        Returns:
+            dict: Parsed result of the prediction.
+        """
+        # Preprocess and prepare input data
+        input_data = np.array(input_resize(input), dtype=np.float32)
 
-# Load TFLite model and allocate tensors.
-interpreter = tf.lite.Interpreter(model_path=weight)
-interpreter.allocate_tensors()
+        # Set the input tensor for the model
+        self.model.set_tensor(self.input_details[0]['index'], input_data)
 
-# Get input and output tensors.
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+        # Run inference
+        self.model.invoke()
+
+        # Retrieve and parse the output
+        output_data = self.model.get_tensor(self.output_details[0]['index'])
+        return parse_result(output_data)
 
 
+class PoolManager:
+    """
+    A thread-safe object pool for managing EmotionClassifier instances.
+    """
+    def __init__(self, pool_size=5):
+        """
+        Initialize the facex.
 
+        Args:
+            pool_size (int): Number of EmotionClassifier instances in the pool.
+        """
+        self.model_path = os.path.join(pathlib.Path(__file__).parent.absolute(), "model_optimized.tflite")
+        self.pool_size = pool_size
+        self.lock = threading.Lock()
+        self.pool = [EmotionClassifier(self.model_path) for _ in range(pool_size)]
+        self.executor = ThreadPoolExecutor(max_workers=pool_size)
 
+    def get_classifier(self):
+        """
+        Acquire a classifier from the pool.
+        
+        Returns:
+            EmotionClassifier: A classifier instance.
+        """
+        with self.lock:
+            if not self.pool:
+                raise RuntimeError("All EmotionClassifier instances are in use!")
+            return self.pool.pop()
 
-    
-def predict(img):
-    
-    input_data = np.array(_preprocess_img(img), dtype=np.float32)
-    interpreter.set_tensor(input_details[0]['index'], input_data)
-    interpreter.invoke()
+    def release_classifier(self, classifier):
+        """
+        Release a classifier back into the pool.
+        
+        Args:
+            classifier (EmotionClassifier): The classifier to release.
+        """
+        with self.lock:
+            self.pool.append(classifier)
 
-    output_data = interpreter.get_tensor(output_details[0]['index'])
-    return _parse_result(output_data)
+    def predict(self, input_data):
+        """
+        Use a classifier from the pool to make a prediction in a thread-safe way.
+
+        Args:
+            input_data (np.ndarray): Input data for the classifier.
+
+        Returns:
+            dict: Parsed result of the prediction.
+        """
+        classifier = self.get_classifier()
+        try:
+            # Perform the prediction using ThreadPoolExecutor
+            future = self.executor.submit(classifier.predict, input_data)
+            return future.result()
+        finally:
+            # Ensure the classifier is returned to the pool
+            self.release_classifier(classifier)
+
+    def shutdown(self):
+        """
+        Clean up the executor and free resources.
+        """
+        self.executor.shutdown(wait=True)
+
--- facex/utils.py
+++ facex/utils.py
@@ -1,7 +1,9 @@
 from skimage.transform import resize
 import numpy as np 
+import tensorflow as tf
 
-def _preprocess_img(raw):
+
+def input_resize(raw):
     img = resize(raw,(200,200, 3))
     img = np.expand_dims(img,axis=0)
     if(np.max(img)>1):
@@ -9,6 +11,15 @@ def _preprocess_img(raw):
     return img
 
 
-def _parse_result(res):
+def parse_result(res):
     cat = ['anger', 'disgust', 'fear', 'happy', 'neutral', 'sadness', 'surprised']
     return dict(zip(cat, res[0]))
+
+
+def create_interpreter(model_path):
+    """
+    Creates and allocates a new TFLite interpreter instance.
+    """
+    interpreter = tf.lite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+    return interpreter
